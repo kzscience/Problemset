4a5129fa541339a221ed59b6c4ec6922ad3d94a8
Нуркалиев Динмухамед (QypshaQ) <54706824+kzscience@users.noreply.github.com>
2021-12-07 21:43:26 +0600

    Update Palindrome Number.cpp

--- C++/17/LeetCode/Palindrome Number.cpp
+++ C++/17/LeetCode/Palindrome Number.cpp
@@ -3,7 +3,13 @@ public:
     bool isPalindrome(const int number) {
         int reverse = 0;
         for (int i = number; i > 0; i /= 10) {
-            reverse = reverse * 10 + i % 10;
+            if (reverse < INT_MAX / 10 || (reverse == INT_MAX / 10 && i / 10 <= INT_MAX % 10)) {
+                reverse = reverse * 10 + i % 10;
+            }
+            else {
+                return false;
+            }
+            
         }
 
         return (number == reverse);

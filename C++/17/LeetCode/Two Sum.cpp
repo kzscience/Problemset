a7920d17ae39da41d008286b9af4c06fb634fa02
Нуркалиев Динмухамед (QypshaQ) <54706824+kzscience@users.noreply.github.com>
2021-12-07 21:34:23 +0600

    Update Two Sum.cpp

--- C++/17/LeetCode/Two Sum.cpp
+++ C++/17/LeetCode/Two Sum.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    vector<int> twoSum(const vector<int>& nums, int target) {
-        unordered_map<int, int> valueToIndex;
-        for (int i = 0; i < (int)nums.size(); i++) {
-            auto it = valueToIndex.find(target - nums[i]);
-            if (it != valueToIndex.end()) {
-                return { it->second, i };
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> m;
+        vector<int> v;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m.find(target-nums[i])!=m.end())
+            {
+                v.push_back(m[target-nums[i]]);
+                v.push_back(i);
+                return v;
             }
-            valueToIndex[nums[i]] = i;
+            m[nums[i]]=i;
         }
-        throw invalid_argument("sum not found");
+        return v;
     }
 };
-
-
-/* Overflow Check
-class Solution {
-private:
-    bool canSubtract(int a, int b) {
-        return (b > 0 && a >= INT_MIN + b) || (b < 0 && a <= INT_MAX + b);  
-    }
-
-public:
-    vector<int> twoSum(const vector<int>& nums, int target) {
-        unordered_map<int, int> valueToIndex;
-        for (int i = 0; i < (int)nums.size(); i++) {
-            if (canSubtract(target, nums[i])) {
-                auto it = valueToIndex.find(target - nums[i]);
-                if (it != valueToIndex.end()) {
-                    return { it->second, i };
-                }
-            }
-            valueToIndex[nums[i]] = i;
-        }
-        throw invalid_argument("sum not found");
-    }
-};
-
-*/

19ac56df21b1a7c9bd8dff2bf1c88a146f611acc
Нуркалиев Динмухамед (QypshaQ) <54706824+kzscience@users.noreply.github.com>
2021-12-07 17:33:13 +0600

    Update Two Sum.cpp

--- C++/17/LeetCode/Two Sum.cpp
+++ C++/17/LeetCode/Two Sum.cpp
@@ -12,3 +12,29 @@ public:
         throw invalid_argument("sum not found");
     }
 };
+
+
+/* Overflow Check
+class Solution {
+private:
+    bool canSubtract(int a, int b) {
+        return (b > 0 && a >= INT_MIN + b) || (b < 0 && a <= INT_MAX + b);  
+    }
+
+public:
+    vector<int> twoSum(const vector<int>& nums, int target) {
+        unordered_map<int, int> valueToIndex;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (canSubtract(target, nums[i])) {
+                auto it = valueToIndex.find(target - nums[i]);
+                if (it != valueToIndex.end()) {
+                    return { it->second, i };
+                }
+            }
+            valueToIndex[nums[i]] = i;
+        }
+        throw invalid_argument("sum not found");
+    }
+};
+
+*/

1de4d2a87d0a9ce38a2c44daffb94e3749d9c8d3
Нуркалиев Динмухамед (QypshaQ) <54706824+kzscience@users.noreply.github.com>
2021-12-15 23:12:43 +0600

    Update 1.1 Connectivity.cpp

--- C++/17/Sedgewick Robert - Algorithm C++/1.1 Connectivity.cpp
+++ C++/17/Sedgewick Robert - Algorithm C++/1.1 Connectivity.cpp
@@ -1,30 +1,27 @@
 //  Give the output that the connectivity algorithm should produce for input pairs 0-2, 1-4, 2-5, 3-6, 0-4, 6-0, and 1-3.
 #include <iostream>
+#include <array>
+#include <numeric>
 
 int main() {
-    int p, q, size;
-    std::cin >> size;
-    int* arr = new int[size];
-
-    for (int i = 0; i < size; i++) {
-        arr[i] = i;
-    }
-
+    const int size = 10;
+    int p, q;
+    std::array<int, size> id;
+    std::iota(id.begin(), id.end(), 0);
     while (std::cin >> p >> q) {
-        int tmp = arr[p];
-
-        for (int i = 0; i < size; i++) {
-            if (arr[i] == tmp) {
-                arr[i] = arr[q];
+        int tmp = id[p];
+        for (int i = 0; i < size; ++i) {
+            if (id[i] == tmp) {
+                id[i] = id[q];
             }
         }
-
-        std::cout << "  " << p << "-" << q << " : ";
+           
+        std::cout << " " << p << " " << q << std::endl;
 
         for (int j = 0; j < size; j++) {
-            std::cout << arr[j] << " ";
+            std::cout << id[j] << " ";
         }
         std::cout << std::endl;
     }
-    delete[]arr;
+
 }

260d6bfa0dbc94e2e9e4032f47b7a38a239cd20b
QypshaQ <QypshaQ777@gmail.com>
2022-02-28 22:51:43 +0600

    Create 111. Minimum Depth of Binary Tree.cpp

---
+++ C++/17/LeetCode/111. Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,28 @@
+const int ZERO = []() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+		if (root == nullptr) {
+			return 0;
+		}
+
+		if (root->left == nullptr && root->right == nullptr) {
+			return 1;
+		}
+
+		if (root->left == nullptr) {
+			return minDepth(root->right) + 1;
+		}
+
+		if (root->right == nullptr) {
+			return minDepth(root->left) + 1;
+		}
+
+		return std::min(minDepth(root->left), minDepth(root->right)) + 1;
+	}
+};
